2d0b40557cfd733c80a8fef4aa50d21140f68060
Osama Kamal <osamakamal650@gmail.com>
2026-02-03 19:34:33 +0500

    practice project

---
+++ main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_projects/Technical%20Text/provider/counter_provider.dart';
+import 'package:firebase_projects/Technical%20Text/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.white,
+          toolbarHeight: 80,
+        ),
+        fontFamily: 'Poppins',
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
